9784144da5caa4c0e5ab063d240abc58cb916cf1
김기진 <abjin69@gmail.com>
2024-11-12 13:13:26 +0900

    feat: add http module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    { ...HttpModule.register({}), global: true },
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
