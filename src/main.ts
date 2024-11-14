4502ad1120aabdf3c9d40ed5771a425199d50529
김기진 <abjin69@gmail.com>
2024-11-14 14:38:01 +0900

    feat: cluster mode

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { useSwagger } from './swagger';
 
+import * as _cluster from 'cluster';
+import * as os from 'os';
+const cluster = _cluster as unknown as _cluster.Cluster;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
@@ -12,7 +16,21 @@ async function bootstrap() {
   configService.getOrThrow('NODE_ENV') !== 'production' && useSwagger(app);
 
   app.enableShutdownHooks();
-  await app.listen(port);
+
+  if (
+    cluster.isPrimary &&
+    configService.getOrThrow('NODE_ENV') !== 'development'
+  ) {
+    const cpuCounts = os.cpus().length;
+    console.log(`Master ${process.pid},  Cpu Counts:${cpuCounts}`);
+
+    for (let i = 0; i < os.cpus().length; i++) {
+      console.log('Cluster: ', i);
+      cluster.fork();
+    }
+  } else {
+    await app.listen(port);
+  }
   return { port, data: new Date() };
 }
 bootstrap().then(console.log).catch(console.log);
