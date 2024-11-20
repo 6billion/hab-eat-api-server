6b3ca983009f222a449b191bf20e61460a2c4cb1
김기진 <abjin69@gmail.com>
2024-11-20 14:01:06 +0900

    feat: add event emitter module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,18 @@ import { HttpModule } from '@nestjs/axios';
 import { DbModule } from './db/db.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ChallengesModule } from './challenges/challenges.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     { ...HttpModule.register({}), global: true },
+    EventEmitterModule.forRoot(),
     DbModule,
     UsersModule,
     AuthModule,
+    ChallengesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
