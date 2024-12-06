9fd43394ca122add63c95a0ea1788884a85a1098
김기진 <abjin69@gmail.com>
2024-12-06 13:11:53 +0900

    feat: 스웨거 수정

--- src/challenges/dtos/get-presigned-url.dto.ts
+++ src/challenges/dtos/get-presigned-url.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsOptional } from 'class-validator';
 
 export class GetPresignedUrlRequestDto {
-  @ApiProperty({ description: '개수', type: Number })
+  @ApiProperty({ description: '개수', type: Number, required: false })
   @IsNumber()
   @IsOptional()
   count?: number = 1;
