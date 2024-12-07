a8975ca3ee70eba368866d4d121b59bdb6853469
chae-ring <149919388+chae-ring@users.noreply.github.com>
2024-12-07 23:01:52 +0900

    module

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,7 @@ import { UtilModule } from '@lib/util';
 import { S3Module } from '@lib/s3';
 import { DietsModule } from './diets/diets.module';
 import { FoodsModule } from './foods/foods.module';
+import { ImageSearchModule } from './image-search-ai/image-search-ai.module';
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import { FoodsModule } from './foods/foods.module';
     ChallengesModule,
     DietsModule,
     FoodsModule,
+    ImageSearchModule,
   ],
   controllers: [AppController],
   providers: [AppService],
