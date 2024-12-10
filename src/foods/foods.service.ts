7826cc7265d6a42824498b9411307d230298271d
chae-ring <149919388+chae-ring@users.noreply.github.com>
2024-12-10 16:17:59 +0900

    keyword mapping

--- src/foods/foods.service.ts
+++ src/foods/foods.service.ts
@@ -40,10 +40,103 @@ export class FoodsService {
 
     return result;
   }
-
+  private readonly labels = [
+    '바베큐',
+    '백숙',
+    '바게트',
+    '반미',
+    '비프 타르타르',
+    '비빔밥',
+    '빙수',
+    '불고기',
+    '분짜',
+    '버거',
+    '부리토',
+    '케이크',
+    '카놀리',
+    '카프레제',
+    '치킨',
+    '칠리게',
+    '초콜릿',
+    '추로스',
+    '뜨거운 커피',
+    '아이스 커피',
+    '쿠키',
+    '크레페',
+    '크로와상',
+    '크로크 무슈',
+    '카레',
+    '다쿠아즈',
+    '딤섬',
+    '도넛',
+    '에그 베네딕트',
+    '에그 타르트',
+    '에스카르고',
+    '피시 앤 칩스',
+    '퐁듀',
+    '프렌치 프라이',
+    '프렌치 토스트',
+    '갈비',
+    '젤라토',
+    '김밥',
+    '그라탱',
+    '핫도그',
+    '훠궈',
+    '자장면',
+    '잡채',
+    '카야 토스트',
+    '케밥',
+    '김치찌개',
+    '한국식 팬케이크',
+    '라자냐',
+    '랍스터',
+    '마카롱',
+    '마들렌',
+    '마파두부',
+    '밀푀유',
+    '머핀',
+    '난',
+    '나쵸',
+    '나시고랭',
+    '오믈렛',
+    '오니기리',
+    '팟타이',
+    '파에야',
+    '팬케이크',
+    '파스타',
+    '파이',
+    '피자',
+    '팝콘',
+    '포크찹',
+    '파운드케이크',
+    '푸딩',
+    '케사디야',
+    '라멘',
+    '라따뚜이',
+    '쌀국수',
+    '리조또',
+    '샐러드',
+    '샌드위치',
+    '사시미',
+    '슈바인학세',
+    '미역국',
+    '시리얼',
+    '소바',
+    '쏨땀',
+    '수프',
+    '스테이크',
+    '스시',
+    '타코',
+    '타코야끼',
+    '티라미수',
+    '또르띠야',
+    '떡볶이',
+    '우동',
+    '와플',
+    '월남쌈',
+  ];
   private readonly dietsImageBaseUrl =
     this.configService.getOrThrow('S3_BASE_URL');
-
   async getImageNameFromAi(key: string): Promise<string> {
     const aiServerUrl = this.configService.getOrThrow(
       'AI_SERVER_FOOD_PREDICT_URL',
@@ -54,7 +147,9 @@ export class FoodsService {
       top1ClassName: string;
     }>(aiServerUrl, { url: imageUrl });
 
-    return response?.data?.top1ClassName;
+    return this.labels.includes(response?.data?.top1ClassName)
+      ? response.data.top1ClassName
+      : 'Unknown';
   }
   getPreSignedUrls(userId: number, count = 1) {
     const today = this.util.getKSTDate();
