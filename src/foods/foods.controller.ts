e366269307bbfcded2313dacf9a93c5c57b13fa6
chae-ring <149919388+chae-ring@users.noreply.github.com>
2024-12-12 20:37:56 +0900

    modify foods

--- src/foods/foods.controller.ts
+++ src/foods/foods.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Query,
-  Param,
-  UseGuards,
-  Post,
-  Body,
-} from '@nestjs/common';
+import { Controller, Get, Query, UseGuards, Post, Body } from '@nestjs/common';
 import { FoodsService } from './foods.service';
 import {
   ApiTags,
