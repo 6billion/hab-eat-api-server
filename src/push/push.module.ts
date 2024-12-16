cf66863832eaf16bcaca6941ea02056c7c2ea1c7
chae-ring <149919388+chae-ring@users.noreply.github.com>
2024-12-16 16:15:38 +0900

    FirebaseAdminModule, push로 이동

--- src/push/push.module.ts
+++ src/push/push.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PushController } from './push.controller';
 import { DietPushService } from './services/diet-push.service';
-import { FirebaseAdminModule } from '../firebase-admin/firebase-admin.module';
 import { WeekChallengeAccomplishedPushService } from './services/week-challenge-accomplished-push.service';
+import { BulkChallengeSuccessPushService } from './services/bulk-chaellenge-success-push.service';
+import { FirebaseAdminService } from './services/firebase-admin.service';
 
 @Module({
-  imports: [FirebaseAdminModule],
   controllers: [PushController],
-  providers: [DietPushService, WeekChallengeAccomplishedPushService],
+  providers: [
+    DietPushService,
+    WeekChallengeAccomplishedPushService,
+    BulkChallengeSuccessPushService,
+    FirebaseAdminService,
+  ],
 })
 export class PushModule {}
