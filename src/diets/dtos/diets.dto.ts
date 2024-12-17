bbdc33e032c5b52efabad6c588bb9f693fb6baa4
김기진 <abjin69@gmail.com>
2024-12-17 13:58:27 +0900

    fix dto

--- src/diets/dtos/diets.dto.ts
+++ src/diets/dtos/diets.dto.ts
@@ -2,10 +2,6 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsDate } from 'class-validator';
 
 export class GetDailyAccumulationDto {
-  @ApiProperty({ description: 'user ID', type: Number })
-  @IsNumber()
-  userId: number;
-
   @ApiProperty({ description: 'date (YYYY-MM-DD)', type: Date })
   @IsDate()
   date: Date;
