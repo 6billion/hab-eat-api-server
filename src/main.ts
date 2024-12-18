fefa6135829ee14a096f3c9d63b72bbf6c3bc914
김기진 <abjin69@gmail.com>
2024-12-18 22:54:37 +0900

    fix port

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
-  const port = configService.get('PORT') || 8080;
+  const port = configService.get('PORT') || 3000;
 
   app.enableShutdownHooks();
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
